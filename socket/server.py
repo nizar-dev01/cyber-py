6b9e8e09c095c4769c77b6b8bebaccd0e971149b
Nizar K <nizar.dev01@gmail.com>
2020-12-31 00:47:18 +0530

    server module

--- socket/server.py
+++ socket/server.py
@@ -18,24 +18,29 @@ def handle_client(conn, address):
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
-        msg_length = int(msg_length)
-        msg = conn.recv(msg_length).decode(FORMAT)
-        if msg == DISCONNECT_MESSAGE:
-            connected = False
-        print(f"[{address}] {conn}")
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+            print(f"Message from client : {msg}")
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+                conn.send("Closing the connection".encode(FORMAT))
+            else:
+                conn.send("MSG received".encode(FORMAT))
 
+    print('Closing Connection')
     conn.close()
 
 
-def start():
-    server.listen()
-    # print(f"[LISTENING] Server is listening on {SERVER}")
+def start(srv):
+    srv.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
     while True:
-        conn, address = server.accept()
+        conn, address = srv.accept()
         thread = threading.Thread(target=handle_client, args=(conn, address))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
 
 
 print('[STARTING] server is starting...')
-start()
+start(server)
