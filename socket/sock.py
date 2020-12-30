08135b270040f09036f6dc8ea7384e6ec19f44cc
Nizar K <nizar.dev01@gmail.com>
2020-12-30 21:20:25 +0530

    socket listener

--- socket/sock.py
+++ socket/sock.py
@@ -4,3 +4,14 @@ s = socket.socket(
     socket.AF_INET, # the kind of socket that we wanna implement
     socket.SOCK_STREAM # the protocol. this one is for TCP. For UDP, we can use SOCK_DGRAM
 )
+s.bind((
+    '127.0.0.1',
+    5555
+))
+s.listen()
+
+while True:
+    client, address = s.accept()
+    print(f"connected to {address}")
+    client.send("You are connected".encode())
+    client.close()
