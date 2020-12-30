f21654b4869dea854d071b88679b9a581eaf0a4c
Nizar K <nizar.dev01@gmail.com>
2020-12-31 00:47:28 +0530

    client module

--- socket/client.py
+++ socket/client.py
@@ -1,12 +1,34 @@
 import socket
 
-s = socket.socket(
-    socket.AF_INET,
-    socket.SOCK_STREAM
-)
+HEADER = 64
+PORT = 5050
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.43.233"
+ADDRESS = (SERVER, PORT)
 
-s.connect(('127.0.0.1',5555))
-msg = s.recv(1024)
-s.close()
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDRESS)
 
-print(msg.decode())
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(1024).decode(FORMAT))
+
+
+def ask():
+    message = input('Enter the message : ')
+    if len(message) > 1:
+        send(message)
+        send(DISCONNECT_MESSAGE)
+    else:
+        ask()
+
+
+send('Test Message')
+ask()
